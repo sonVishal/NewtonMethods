5289e722e025d75b28abab36da66d49403973820
sonVishal <vishal.sontakke@tum.de>
2016-05-26 09:19:58 +0200

    Update n1fact and n1solv

--- Helper.jl
+++ Helper.jl
@@ -90,3 +90,42 @@ function n1scrb(n,lda,ml,mu,a)
     end
     return (aout,fw)
 end
+
+function n1fact(n,lda,ml,mu,a,opt)
+    # Begin
+    mStor = opt.options[OPT_MSTOR]
+    if mStor == 0
+        try
+            (l,u,p) = lu(a)
+            iFail = 0
+        catch
+            iFail = 1
+        end
+    elseif mStor == 1
+        # Band mode: l holds the complete lu-factorization of p*a
+        l = a[:,:]
+        # Use LINPACK function to compute the LU in place
+        # p stores the pivot vectors and not the
+        (l,p) = gbtrf!(ml,mu,lda,l)
+        u = []
+    end
+    if iFail != 0
+        iFail = 1
+    end
+    return (l,u,p,iFail)
+end
+
+function n1solv(n,lda,ml,mu,l,u,p,b,opt)
+    # Begin
+    mStor = opt.options[OPT_MSTOR]
+    if mStor == 0
+        x = p*b
+        x = l\x
+        x = u\x
+    elseif mStor == 1
+        # TODO: Check this. Should be "a" in place of "l"
+        gbtrs!(N,ml,mu,lda,l,p,b)
+    end
+    iFail = 0
+    return (x,iFail)
+end

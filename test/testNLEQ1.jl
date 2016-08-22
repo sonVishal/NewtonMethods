7c9c2c6ed1fe855450523861a571dde353cdf257
sonVishal <sontakke.vishal1991@gmail.com>
2016-08-22 11:06:55 +0200

    Add test for NLEQ1

--- test/testNLEQ1.jl
+++ test/testNLEQ1.jl
@@ -1,5 +1,45 @@
-using Base.Test
-include("../examples/ChebyQuad.jl");
+function chebyQuad(x,f)
+    n = length(x)
+    f[:] = zeros(n);
+    for i = 2:2:n
+        f[i] = n/(i*i-1)
+    end
+    for l = 1:n
+        factt = 4.0*x[l] - 2.0
+        ti2 = 1.0
+        ti1 = 0.5*factt
+        f[1] = ti1 + f[1]
+        for i = 2:n
+            ti = factt*ti1-ti2
+            f[i] = ti + f[i]
+            ti2 = ti1
+            ti1 = ti
+        end
+    end
+    return nothing
+end
+
+function chebyQuadJac(x,J)
+    n = length(x)
+    for j = 1:n
+        factt = 4.0*x[j] - 2.0
+        ti2 = 1.0
+        ti1 = 0.5*factt
+        tabli2 = 0.0
+        tabli1 = 2.0
+        J[1,j] = tabli1
+        for i = 2:n
+            ti = factt*ti1-ti2
+            tabli = 4.0*ti1+factt*tabli1-tabli2
+            J[i,j] = tabli
+            ti2 = ti1
+            ti1 = ti
+            tabli2 = tabli1
+            tabli1 = tabli
+        end
+    end
+    return nothing
+end
 
 function testNLEQ1()
     refSol = Dict{Int32,Any}();
@@ -55,6 +95,7 @@ function testNLEQ1()
                 0.80050932769225;
                 0.955794653864221]
 
+    testResult = true;
     for dim = 2:9
         n1 = dim + 1
 
@@ -73,19 +114,15 @@ function testNLEQ1()
         retCode = -1
         stats   = []
 
-        @printf("Testing NLEQ1 using ChebyQuad of dimension %d ",dim)
         while retCode == -1
             (x0, stats, retCode) = nleq1(chebyQuad,x0,xScal,opt)
         end
 
         relNormDiff = norm(x0-refSol[dim])/norm(refSol[dim])
-        try
-            @test relNormDiff <= 1e-12
-            @printf("successful.\n")
-        catch
-            @printf("failed.\n")
-        end
+
+        testResult &= relNormDiff <= 1e-12
     end
+    return testResult
 end
 
 testNLEQ1()

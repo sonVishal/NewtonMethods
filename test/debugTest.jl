7fb988cb131691b53275c0220eb57033c5d44e1e
Vishal Sontakke <vishal.sontakke@tum.de>
2016-10-07 18:37:03 +0200

    Go back to dim=2

--- test/debugTest.jl
+++ test/debugTest.jl
@@ -20,41 +20,40 @@ refSol[9] = [0.4420534614e-1, 0.1994906723, 0.2356191085, 0.4160469079,
                 0.5000000000, 0.5839530921, 0.7643808915, 0.8005093277,
                 0.9557946539]
 
-# for dim = 2:9
 dim = 2
-    n1 = dim + 1
+n1 = dim + 1
 
-    # Initialize the options
-    opt = OptionsNLEQ(OPT_MODE              => 1,
-                      OPT_JACGEN            => 1,
-                      OPT_PRINTWARNING      => 1,
-                      OPT_PRINTITERATION    => 3,
-                      OPT_PRINTSOLUTION     => 2,
-                      OPT_PRINTIOWARN       => fRest,
-                      OPT_PRINTIOMON        => fRest,
-                      OPT_PRINTIOSOL        => fSol,
-                      OPT_JACFCN            => chebyQuadJac,
-                      OPT_MSTOR             => 0,
-                      OPT_NOROWSCAL         => 0,
-                      OPT_NITMAX            => 200,
-                      OPT_RTOL              => 1e-5)
+# Initialize the options
+opt = OptionsNLEQ(OPT_MODE              => 1,
+                  OPT_JACGEN            => 1,
+                  OPT_PRINTWARNING      => 1,
+                  OPT_PRINTITERATION    => 3,
+                  OPT_PRINTSOLUTION     => 2,
+                  OPT_PRINTIOWARN       => fRest,
+                  OPT_PRINTIOMON        => fRest,
+                  OPT_PRINTIOSOL        => fSol,
+                  OPT_JACFCN            => chebyQuadJac,
+                  OPT_MSTOR             => 0,
+                  OPT_NOROWSCAL         => 0,
+                  OPT_NITMAX            => 200,
+                  OPT_RTOL              => 1e-5)
 
-    x0    = collect(1:dim)./n1
-    xScal = zeros(x0)
+x0    = collect(1:dim)./n1
+xScal = zeros(x0)
 
-    retCode = -1
+retCode = -1
 
-    i = 1
+i = 1
 
-    while retCode == -1
-        (x0, _, retCode) = nleq2(chebyQuad, x0, xScal, opt)
-        write(fRest, @sprintf("Returned from call %4i of NLEQ2\n",i))
-        flush(fSol)
-        flush(fRest)
-        i += 1
-    end
-    err = norm(x0-refSol[dim],Inf)/norm(refSol[dim],Inf)
-    println("Relative error in Inf norm = $err")
-# end
+while retCode == -1
+    (x0, _, retCode) = nleq2(chebyQuad, x0, xScal, opt)
+    write(fRest, @sprintf("Returned from call %4i of NLEQ2\n",i))
+    flush(fSol)
+    flush(fRest)
+    i += 1
+end
+err = norm(x0-refSol[dim],Inf)/norm(refSol[dim],Inf)
+println("Relative error in Inf norm = $err")
+
 close(fSol)
 close(fRest)
